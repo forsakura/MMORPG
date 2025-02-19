c17fce3179092922d97aef268978e6ea7a0430eb
YaeSakura <1152438234@qq.com>
2025-02-19 14:07:27 +0800

    add 商店系统服务端部分

--- Src/Server/GameServer/GameServer/Managers/ShopManager.cs
+++ Src/Server/GameServer/GameServer/Managers/ShopManager.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Common;
+using Common.Data;
+using GameServer.Services;
+using Network;
+using SkillBridge.Message;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GameServer.Managers
 {
-    internal class ShopManager
+    internal class ShopManager : Singleton<ShopManager>
     {
+        internal Result BuyItem(NetConnection<NetSession> sender, int shopId, int shopItemId)
+        {
+            if(DataManager.Instance.Shops.ContainsKey(shopId))
+            {
+                ShopItemDefine shopItemDefine = null;
+                if (DataManager.Instance.ShopItems[shopId].TryGetValue(shopItemId, out shopItemDefine))
+                {
+                    Log.InfoFormat("BuyItem:: character: {0} item : id: {1} count: {2} price: {3}", sender.Session.Character, shopItemDefine.itemID, shopItemDefine.Count, shopItemDefine.Price);
+                    if(sender.Session.Character.Gold >= shopItemDefine.Price)
+                    {
+                        sender.Session.Character.ItemManager.Add(shopItemDefine.itemID, shopItemDefine.Count);
+                        sender.Session.Character.Gold -= shopItemDefine.Price;
+                        DBService.Instance.Save();
+                        return Result.Success;
+                    }
+                }
+            }
+            return Result.Failed;
+        }
     }
 }
--- Src/Server/GameServer/GameServer/Services/ItemService.cs
+++ Src/Server/GameServer/GameServer/Services/ItemService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Common;
+using GameServer.Managers;
+using Network;
+using SkillBridge.Message;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,25 @@ using System.Threading.Tasks;
 
 namespace GameServer.Services
 {
-    internal class ItemService
+    internal class ItemService : Singleton<ItemService>, IDisposable
     {
+        public ItemService()
+        {
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<ItemBuyRequest>(OnItemBuy);
+        }
+
+        public void Dispose()
+        {
+            MessageDistributer<NetConnection<NetSession>>.Instance.Unsubscribe<ItemBuyRequest>(OnItemBuy);
+        }
+
+        private void OnItemBuy(NetConnection<NetSession> sender, ItemBuyRequest message)
+        {
+            Log.InfoFormat("ItemBuyRequest:: ShopID: {0} ShopItemID: {1}", message.shopId, message.shopItemId);
+            var res = ShopManager.Instance.BuyItem(sender, message.shopId, message.shopItemId);
+            sender.Session.Response.itemBuy = new ItemBuyResponse();
+            sender.Session.Response.itemBuy.Result = res;
+            sender.SendResponse();
+        }
     }
 }
